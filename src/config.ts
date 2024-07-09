d8e39eb2c1374e4ecafd03cc38f738e1a7020a4a
AnMaster15 <119131473+AnMaster15@users.noreply.github.com>
2024-07-09 08:00:38 +0530

    config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvtHsL7XYkEANilq1aQydKL3wAK6Ec4OM",
+  apiKey: "",
   authDomain: "foodapp-2dc59.firebaseapp.com",
   projectId: "foodapp-2dc59",
   storageBucket: "foodapp-2dc59.appspot.com",
-  messagingSenderId: "227110810691",
-  appId: "1:227110810691:web:9045f8e0f24eca9ec1d0f6",
-  measurementId: "G-5PP2E3P8C6"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
