5659385a962d28cff3f5b78a59cf197628d095d6
AnMaster15 <119131473+AnMaster15@users.noreply.github.com>
2024-07-09 08:00:06 +0530

    firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAvtHsL7XYkEANilq1aQydKL3wAK6Ec4OM",
+    apiKey: "",
     authDomain: "foodapp-2dc59.firebaseapp.com",
     projectId: "foodapp-2dc59",
     storageBucket: "foodapp-2dc59.appspot.com",
-    messagingSenderId: "227110810691",
-    appId: "1:227110810691:web:9045f8e0f24eca9ec1d0f6",
-    measurementId: "G-5PP2E3P8C6"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 if (!firebase.apps.length) {
