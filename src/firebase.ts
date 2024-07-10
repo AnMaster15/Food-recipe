d5dfc794e717e75a3b86013eab4cc48ed555007e
AnMaster15 <119131473+AnMaster15@users.noreply.github.com>
2024-07-10 18:08:41 +0530

    firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "foodapp-2dc59.firebaseapp.com",
-    projectId: "foodapp-2dc59",
-    storageBucket: "foodapp-2dc59.appspot.com",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
